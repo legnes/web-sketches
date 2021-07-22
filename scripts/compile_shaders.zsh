6a1cddf359d71254a630dbcdad1e4f4784df2456
Sam <samuel.d.engel@gmail.com>
2021-07-22 16:50:44 -0400

    Move public to root for gh pages

--- scripts/compile_shaders.zsh
+++ scripts/compile_shaders.zsh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-rm -f public/shaders/webgpu/built/*
-for file in public/shaders/webgpu/*.{vert,frag,comp}(N); glslangValidator -V $file -o ${file:s/webgpu/webgpu\/built}.spv
+rm -f shaders/webgpu/built/*
+for file in shaders/webgpu/*.{vert,frag,comp}(N); glslangValidator -V $file -o ${file:s/webgpu/webgpu\/built}.spv
